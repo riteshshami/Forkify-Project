7da8e583c01855e91d9da5367d87716d459b7689
Ritesh <ritesh@enablero.com>
2024-07-06 07:58:17 +0530

    Project Complete

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ac1c7c97-3f2e-4b9f-a32b-2b46c02bf3d3';
+export const MODAL_CLOSE_SEC = 2.5;
